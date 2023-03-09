986713d93b63b30fa4cad4f8a9d9a5648db2cf36
amanrjgit <111105723+amanrjgit@users.noreply.github.com>
2023-03-09 09:40:16 +0530

    Update main.py

--- Instagram/main.py
+++ Instagram/main.py
@@ -2,8 +2,22 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import base64
+import plotly.graph_objects as go
+from instagrapi import Client
 
-main_bg = "instagram/instagram-bg2.avif"
+#client = Client()
+#client.login("test_ing4747", "testing123")
+
+def details(username):
+    #dict(client.user_info_by_username(username))
+    pass
+
+
+st.set_page_config(page_title="InstaBot Identifier- Aman Kumar Jaiswar",layout="wide")
+
+# image
+
+main_bg = "bg.jpg"
 main_bg_ext = "jpg"
 
 with open(main_bg, "rb") as image_file:
@@ -20,9 +34,43 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+hide_img_fs = '''
+<style>
+button[title="View fullscreen"]{
+    visibility: hidden;}
+</style>
+'''
 
-st.title("InstaBot Identifier")
+fig = go.Figure(go.Indicator(
+    mode = "gauge+number",
+    value = 60,
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    title = {'text': "Fakeness", 'font': {'size': 50}},
+    delta = {'reference': 400, 'increasing': {'color': "RebeccaPurple"}},
+    gauge = {
+        'axis': {'range': [None, 100], 'tickwidth': 1, 'tickcolor': "darkblue"},
+        'bar': {'color': "darkblue"},
+        'bgcolor': "white",
+        'borderwidth': 2,
+        'bordercolor': "gray",
+        'steps': [
+            {'range': [0, 250], 'color': 'cyan'},
+            {'range': [250, 400], 'color': 'royalblue'}],
+        'threshold': {
+            'line': {'color': "red", 'width': 4},
+            'thickness': 0.75,
+            'value': 490}}))
+
+fig.update_layout(paper_bgcolor = "lavender", font = {'color': "darkblue", 'family': "Arial"})
+
+
 
 col1, col2 = st.columns([1,1],gap="small")
+with col1:
+    st.image("insta.png")
+    st.markdown(hide_img_fs, unsafe_allow_html=True)
+
 with col2:
-    input_data = st.text_input(label="Enter Username")
+    st.title("InstaBot Identifier")
+    input_data = st.text_input(label="Search for a username")
+    st.plotly_chart(fig)

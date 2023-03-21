47cacf7c97fda0fc6fd48b8358c67f8be049b2ef
amanrjgit <111105723+amanrjgit@users.noreply.github.com>
2023-03-21 13:21:54 +0530

    Update main.py

--- Face_detection/main.py
+++ Face_detection/main.py
@@ -1,26 +1,46 @@
+import numpy as np
 import cv2
-import pandas as pd
+import pickle
 
+face_cascade = cv2.CascadeClassifier('casscade/haarcascade_frontalface_alt2.xml')
 
-face = cv2.CascadeClassifier('casscade/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+#recognizer.read("face-trainer.yml")
+#recognizer.read("face-trainer2.yml")
+recognizer.read("face-trainer-default.yml")
+
+labels = {}
+with open("face-labels.pickle", 'rb') as f:
+	labels = pickle.load(f)
 
 cap = cv2.VideoCapture(0)
 
-while True:
-    _,frame = cap.read()
-    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face.detectMultiScale(grey, scaleFactor=1.5, minNeighbors=5)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces:
-        # print(x,y,w,h)
-        roi_gray = grey[y:y + h, x:x + w]  # (ycord_start, ycord_end)
-        roi_color = frame[y:y + h, x:x + w]
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
 
-        color = (255, 0, 0)  # BGR 0-255
+        id_, conf = recognizer.predict(roi_gray)
+        if conf>=4 and conf <= 85:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+        color = (255, 0, 0) #BGR 0-255
         stroke = 2
         end_cord_x = x + w
         end_cord_y = y + h
         cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
 
-    cv2.imshow('Virtual Mouse', frame)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
